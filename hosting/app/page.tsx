5d9aeedfd7459fc7acd95bfe371127dc9447eda4
David Kalajdzic <david.kalajdzic@icloud.com>
2024-06-20 00:42:33 +0200

    new project

--- hosting/app/page.tsx
+++ hosting/app/page.tsx
@@ -4,14 +4,23 @@ import { initializeApp } from 'firebase/app';
 import { addDoc, collection, deleteDoc, doc, getDocs, getFirestore } from 'firebase/firestore';
 
 // Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCsP4G2Q3c-d8Xi6ngH8w0Y7AkFuvXJwRc",
+//     authDomain: "unprojettest-cb8ed.firebaseapp.com",
+//     projectId: "unprojettest-cb8ed",
+//     storageBucket: "unprojettest-cb8ed.appspot.com",
+//     messagingSenderId: "153996520538",
+//     appId: "1:153996520538:web:4b586893b8b1ebb7ad7c63",
+//     measurementId: "G-N9JYWTX36Q"
+// };
 const firebaseConfig = {
-    apiKey: "AIzaSyCsP4G2Q3c-d8Xi6ngH8w0Y7AkFuvXJwRc",
-    authDomain: "unprojettest-cb8ed.firebaseapp.com",
-    projectId: "unprojettest-cb8ed",
-    storageBucket: "unprojettest-cb8ed.appspot.com",
-    messagingSenderId: "153996520538",
-    appId: "1:153996520538:web:4b586893b8b1ebb7ad7c63",
-    measurementId: "G-N9JYWTX36Q"
+    apiKey: "AIzaSyCD_bB8seL_PBQX7EpJm_JYi1B1lFqhHs0",
+    authDomain: "deuxieme-projet-test.firebaseapp.com",
+    projectId: "deuxieme-projet-test",
+    storageBucket: "deuxieme-projet-test.appspot.com",
+    messagingSenderId: "341887412320",
+    appId: "1:341887412320:web:1a439c885ea328b6d37b05",
+    measurementId: "G-581H4RZ9K9"
 };
 
 // Initialize Firebase
