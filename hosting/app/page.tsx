8e2736d05d6b2a5f9025b27bfe0324597458e338
David Kalajdzic <david.kalajdzic@icloud.com>
2024-06-20 16:13:57 +0200

    now it compiles

--- hosting/app/page.tsx
+++ hosting/app/page.tsx
@@ -11,7 +11,7 @@ const firebaseConfig = {
     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
     appId: process.env.NEXT_PUBLIC_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_IDD
 };
 // assert env vars exist (firebaseConfig.apiKey)
 Object.keys(firebaseConfig).forEach(key => {
