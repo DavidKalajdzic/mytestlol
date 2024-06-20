dbd072f6e07bd3da097786b1ea9dccf66635f454
David Kalajdzic <david.kalajdzic@icloud.com>
2024-06-20 15:07:48 +0200

    separate them correctly

--- hosting/app/page.tsx
+++ hosting/app/page.tsx
@@ -1,26 +1,25 @@
 "use client";
-import { useEffect, useState } from "react";
-import { initializeApp } from 'firebase/app';
-import { addDoc, collection, deleteDoc, doc, getDocs, getFirestore } from 'firebase/firestore';
+import {useEffect, useState} from "react";
+import {initializeApp} from 'firebase/app';
+import {addDoc, collection, deleteDoc, doc, getDocs, getFirestore} from 'firebase/firestore';
+
+function getEnv(name) {
+    let val = process.env[name];
+    if ((val === undefined) || (val === null)) {
+        throw ("missing env var for " + name);
+    }
+    return val;
+}
 
 // Firebase configuration
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCsP4G2Q3c-d8Xi6ngH8w0Y7AkFuvXJwRc",
-//     authDomain: "unprojettest-cb8ed.firebaseapp.com",
-//     projectId: "unprojettest-cb8ed",
-//     storageBucket: "unprojettest-cb8ed.appspot.com",
-//     messagingSenderId: "153996520538",
-//     appId: "1:153996520538:web:4b586893b8b1ebb7ad7c63",
-//     measurementId: "G-N9JYWTX36Q"
-// };
 const firebaseConfig = {
-    apiKey: "AIzaSyCD_bB8seL_PBQX7EpJm_JYi1B1lFqhHs0",
-    authDomain: "deuxieme-projet-test.firebaseapp.com",
-    projectId: "deuxieme-projet-test",
-    storageBucket: "deuxieme-projet-test.appspot.com",
-    messagingSenderId: "341887412320",
-    appId: "1:341887412320:web:1a439c885ea328b6d37b05",
-    measurementId: "G-581H4RZ9K9"
+    apiKey: getEnv("NEXT_PUBLIC_API_KEY"),
+    authDomain: getEnv("NEXT_PUBLIC_AUTH_DOMAIN"),
+    projectId: getEnv("NEXT_PUBLIC_PROJECT_ID"),
+    storageBucket: getEnv("NEXT_PUBLIC_STORAGE_BUCKET"),
+    messagingSenderId: getEnv("NEXT_PUBLIC_MESSAGING_SENDER_ID"),
+    appId: getEnv("NEXT_PUBLIC_APP_ID"),
+    measurementId: getEnv("NEXT_PUBLIC_MEASUREMENT_ID")
 };
 
 // Initialize Firebase
@@ -34,7 +33,7 @@ const TodoList = () => {
 
     const fetchTodos = async () => {
         const querySnapshot = await getDocs(collection(db, 'todos'));
-        setTodos(querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })));
+        setTodos(querySnapshot.docs.map(doc => ({id: doc.id, ...doc.data()})));
     };
 
     useEffect(() => {
@@ -43,7 +42,7 @@ const TodoList = () => {
 
     const addTodo = async () => {
         if (newTodo.trim() === '') return;
-        await addDoc(collection(db, 'todos'), { text: newTodo });
+        await addDoc(collection(db, 'todos'), {text: newTodo});
         setNewTodo('');
         fetchTodos();
     };
@@ -79,7 +78,7 @@ const TodoList = () => {
 export default function Page() {
     return (
         <div>
-            <TodoList />
+            <TodoList/>
         </div>
     );
 }
