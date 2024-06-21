4c067071556d078e62f30c8bf8fd0cf7ce92741d
David Kalajdzic <david.kalajdzic@icloud.com>
2024-06-21 18:51:05 +0200

    test

--- hosting/app/page.tsx
+++ hosting/app/page.tsx
@@ -52,7 +52,7 @@ const TodoList = () => {
 
     return (
         <div>
-            <h1>QOOOOLLLL {process.env.NEXT_PUBLIC_COUCOU} {process.env.KIKI}</h1>
+            <h1>QOOOOLLLL {process.env.NEXT_PUBLIC_PROJECT_ID} </h1>
             <input
                 type="text"
                 value={newTodo}
